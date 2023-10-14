0e4f12d14c205d3c1238114935a07d07c7f4671f
mgguisoben <mgguisoben@gmail.com>
2023-10-14 23:49:45 +0800

    Functions improvement

--- functions.py
+++ functions.py
@@ -7,6 +7,13 @@ def get_todos():
     return data
 
 
+def todos_list():
+    data = get_todos()
+    new_list = [todo.strip('\n') for todo in data]
+    return new_list
+
+
 def overwrite_todos(data):
+    new_data = [item + '\n' for item in data]
     with open(filepath, 'w') as file:
-        file.writelines(data)
+        file.writelines(new_data)
--- gui.py
+++ gui.py
@@ -1,23 +1,50 @@
-from functions import get_todos, overwrite_todos
+from functions import get_todos, todos_list, overwrite_todos
 from datetime import datetime
 import PySimpleGUI as sg
 
 date_time_now = datetime.strftime(datetime.now(), '%b %d %H:%M:%S')
+todos = todos_list()
+print(todos)
 
 layout = [ [sg.Text(date_time_now)],
            [sg.Text('Enter new To-Do:')],
-           [sg.InputText(do_not_clear=False), sg.Button('Add', size=10)], # input field auto clear
-           [sg.Text("more text"), [sg.Button('Edit'), sg.Button('Done')]],
+           [sg.InputText(key='_INPUT_' ,do_not_clear=False, size=50), sg.Button('Add', size=10)], # input field auto clear
+           [sg.Listbox(key='_LISTBOX_', values=todos, size=(48, 6), enable_events=True),
+            sg.Button('Edit', size=4), sg.Button('Done', size=4)],
            [sg.Button('Quit', size=10)]
          ]
 
-window = sg.Window('My TO-DO App', layout)
+window = sg.Window('My TO-DO App').Layout(layout)
 
 while True:
 
-    events, values = window.read()
+    event, values = window.read()
+    window.Element('_LISTBOX_').update((todos))
 
-    if events == sg.WIN_CLOSED or events == 'Quit':
+    if event == sg.WIN_CLOSED or event == 'Quit':
         break
+
+    if event == 'Add':
+        todo = values['_INPUT_']
+        todos.append(todo)
+        overwrite_todos(todos)
+        window.Element('_LISTBOX_').update((todos))
+
+    if event == '_LISTBOX_' and len(values['_LISTBOX_']):
+        window.Element('_INPUT_').update(values['_LISTBOX_'])
+
+    if event == 'Done':
+        try:
+            todo = values['_INPUT_'][2:-3]
+            index = todos.index(todo)
+            todos.pop(index)
+            window.Element('_LISTBOX_').update((todos))
+        except ValueError:
+            continue
+
+    # if event == 'Edit':
+    #     window.Element('_INPUT_').update(values['_LISTBOX_'])
+    #     sg.Popup('Edit To-DO', values['_INPUT_'])
+
 
 window.close()
