541a90654fd6de06424603cdb88f78e3327d46f8
mgguisoben <mgguisoben@gmail.com>
2023-10-18 18:17:40 +0800

    Create interactive web interface

--- web.py
+++ web.py
@@ -1,15 +1,25 @@
-from functions import todos_list
+from functions import todos_list, overwrite_todos
 import streamlit as st
 
 todos = todos_list()
 
-st.title('To-Do Web App')
-st.subheader('To-do web app using streamlit')
-st.write('Thing you need to do.')
+def add_todo():
+    todo = st.session_state['new_todo']
+    todos.append(todo)
+    overwrite_todos(todos)
 
-for todo in todos:
-    st.checkbox(todo)
 
-st.text_input(label='', placeholder='Add a to-do')
+st.title('TODO')
+st.subheader('Minimalist to-do web app:')
+st.write('Thing you need to do:')
 
-st.button(label='Add')
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(label=todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        overwrite_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label='What do you need to do?', placeholder='Add a to-do',
+              key='new_todo', on_change=add_todo)
